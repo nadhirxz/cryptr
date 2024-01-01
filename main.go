a9880652700c6354fab374c091d692e072e55b04
Nadhir Ferdji <nadhirxz@gmail.com>
2024-01-01 04:01:24 +0100

    Add cli funcionality for file encryption/decryption

--- main.go
+++ main.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/akamensky/argparse"
+	"github.com/nadhirxz/cryptr/utils"
+)
+
+func main() {
+	parser := argparse.NewParser("cryptr", "Encrypt/Decrypt files")
+
+	filename := parser.StringPositional(&argparse.Options{Required: true, Help: "File to encrypt/decrypt"})
+	password := parser.String("p", "password", &argparse.Options{Required: true, Help: "Password to encrypt/decrypt file"})
+	encrypt := parser.Flag("e", "encrypt", &argparse.Options{Required: false, Help: "Encrypt file"})
+	decrypt := parser.Flag("d", "decrypt", &argparse.Options{Required: false, Help: "Decrypt file"})
+
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+		return
+	}
+
+	if *encrypt == *decrypt {
+		fmt.Println("Please choose either encrypt or decrypt")
+		return
+	}
+
+	key := utils.GenerateKey(*password)
+
+	if *encrypt {
+		err := utils.EncryptFile(*filename, key)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("File encrypted")
+		return
+	}
+
+	if *decrypt {
+		err := utils.DecryptFile(*filename, key)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("File decrypted")
+		return
+	}
+}
