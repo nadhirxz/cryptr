d25cfee79181cd594374fdc75ca9c7fa04d5f642
Nadhir Ferdji <nadhirxz@gmail.com>
2024-01-01 04:12:24 +0100

    Add prompt for password if not provided

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+	"syscall"
 
 	"github.com/akamensky/argparse"
 	"github.com/nadhirxz/cryptr/utils"
+	"golang.org/x/term"
 )
 
 func main() {
 	parser := argparse.NewParser("cryptr", "Encrypt/Decrypt files")
 
 	filename := parser.StringPositional(&argparse.Options{Required: true, Help: "File to encrypt/decrypt"})
-	password := parser.String("p", "password", &argparse.Options{Required: true, Help: "Password to encrypt/decrypt file"})
+	password := parser.String("p", "password", &argparse.Options{Required: false, Help: "Password to encrypt/decrypt file"})
 	encrypt := parser.Flag("e", "encrypt", &argparse.Options{Required: false, Help: "Encrypt file"})
 	decrypt := parser.Flag("d", "decrypt", &argparse.Options{Required: false, Help: "Decrypt file"})
 	output := parser.String("o", "output", &argparse.Options{Required: false, Help: "Output file"})
@@ -26,6 +28,16 @@ func main() {
 	if *encrypt == *decrypt {
 		fmt.Println("Please choose either encrypt or decrypt")
 		return
+	}
+
+	if *password == "" {
+		fmt.Print("Password: ")
+		bytepw, err := term.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		*password = string(bytepw)
 	}
 
 	key := utils.GenerateKey(*password)
